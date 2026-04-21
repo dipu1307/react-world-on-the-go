e3313c39f88bc3c432565f6b3d3814f5dbefe63d
unknown <arifsamiha1307@gmail.com>
2026-04-21 18:11:43 +0600

    set the flag in grid and add a button only

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({ country }) => {
-    console.log(country);
+    // console.log(country);
+    const handleVisited=()=>{
+        console.log('button click');
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country?.flags?.flags?.png} alt={country?.flags?.flags?.alt} />
             <h3>Name: {country?.name?.common} </h3>
             <p>Capital: {country?.capital?.capital}</p>
             <p>Population: {country?.population?.population}</p>
+            <p>Area: {country.area.area} {country.area.area>300000? "Big Country":"Small Country"}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
